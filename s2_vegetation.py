8edc025402cb38c44ecf42164e70f4bc96777a4b
balakumaran <balakumaran247@gmail.com>
2021-04-19 17:34:51 +0530

    Added: Sentinelsat - download and extract

--- s2_vegetation.py
+++ s2_vegetation.py
@@ -1,19 +1,10 @@
-from sentinelsat import SentinelAPI
-import geopandas as gpd
-import pandas as pd
-from shapely.geometry import MultiPolygon, Polygon
-import numpy as np
-import os
-
 import input_details
+from download_data import download_data
 
-api = SentinelAPI(input_details.username, input_details.password, 'https://scihub.copernicus.eu/dhus')
+def main():
+    username, password = input_details.username, input_details.password
+    latitude, longitude = input_details.latitude, input_details.longitude
+    download_data(username, password, latitude, longitude)
 
-footprint = f'POINT ({input_details.latitude} {input_details.longitude})'
-print(footprint)
-
-data_populate = api.query(footprint, date=('NOW-12MONTHS','NOW'), order_by='cloudcoverpercentage', platformname='Sentinel-2', processinglevel='Level-2A', cloudcoverpercentage=(0,10))
-print(len(data_populate))
-
-data_database = api.to_geodataframe(data_populate)
-print(data_database)
+if __name__ == '__main__':
+    main()
