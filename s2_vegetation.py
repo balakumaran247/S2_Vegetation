805d590cf3dc957ec7771824e71ff2c1c23121d9
balakumaran <balakumaran247@gmail.com>
2021-05-27 23:50:54 +0530

    Added: indices

--- s2_vegetation.py
+++ s2_vegetation.py
@@ -1,11 +1,12 @@
 import input_details
 from s2vegetation.download_data import download_data
+from s2vegetation.indices import calculate_indices
 
 def main():
     username, password = input_details.username, input_details.password
     latitude, longitude = input_details.latitude, input_details.longitude
     dl_info = download_data(username, password, latitude, longitude)
-    print(dl_info)
+    #calculate_indices()
 
 if __name__ == '__main__':
     main()
